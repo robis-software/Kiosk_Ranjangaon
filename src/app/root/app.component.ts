ed35a5e3290c5b6277fa7eda2f3c44e4545b0efa
Gopalsamy <Vigneswara.Gopalsamy@mate.motherson.com>
2026-04-21 13:09:23 +0530

    Minor changes and Button size increased

--- src/app/root/app.component.ts
+++ src/app/root/app.component.ts
@@ -54,7 +54,7 @@ export class AppComponent implements OnInit {
             const isEmpty = !data || Object.keys(data).length === 0;
             const isNotLive = data?.live === false;
             if((isEmpty || isNotLive) && this.router.url !== '/disconnected') {
-                this.router.navigateByUrl('/disconnected');
+                // this.router.navigateByUrl('/disconnected');
             }
 
             if(this.configuration?.battery.min > this.liveData?.battery) {
